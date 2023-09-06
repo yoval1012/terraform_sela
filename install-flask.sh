c13098d9ed8df8b79a4878acaf1391fe81bb6a6e
yuval leibovich <121787923+yoval1012@users.noreply.github.com>
2023-09-06 18:24:59 +0300

    Update install-flask.sh

--- install-flask.sh
+++ install-flask.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Update the package list
+# Install Flask
 sudo apt-get update
-
-# Install Python 3 and pip
-sudo apt-get install -y python3 python3-pip
-
-# Install Flask using pip
+sudo apt-get install -y python3-pip
 sudo pip3 install Flask
+
+# Create your Flask app (app.py)
+cat <<EOL > app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask on VM1!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+EOL
+
+# Run the Flask app
+python3 app.py
+
 
