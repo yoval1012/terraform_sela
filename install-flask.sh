9dc33a2689a4f8c0b448846ab226251a0dfbd9b8
yuval leibovich <121787923+yoval1012@users.noreply.github.com>
2023-09-07 11:39:44 +0300

    Update install-flask.sh

--- install-flask.sh
+++ install-flask.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-# Install Flask
+# Update system and install required packages
 sudo apt-get update
 sudo apt-get install -y python3-pip
-sudo pip3 install Flask
+sudo pip3 install Flask psycopg2-binary
 
 # Create your Flask app (app.py)
 cat <<EOL > app.py
 from flask import Flask
+import psycopg2
+
+# Database configuration
+db_host = "10.0.2.5"
+db_port = 5432
+db_user = "yuval"
+db_password = "1234567"
+db_name = "db"
+
+# Create a PostgreSQL connection
+try:
+    conn = psycopg2.connect(
+        host="10.0.2.5",
+        port="5432",
+        user="yuval",
+        password="1234567",
+        database="db"
+    )
+    cursor = conn.cursor()
+    print("Connected to PostgreSQL")
+except Exception as e:
+    print(f"Error: {e}")
 
 app = Flask(__name__)
 
@@ -21,5 +43,4 @@ EOL
 
 # Run the Flask app
 nohup python3 app.py > /dev/null 2>&1 &
-
 
